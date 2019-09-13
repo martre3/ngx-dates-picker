721f3a464fd276517eb6a9def6afb4d9aeabec97
Martynas Treinys <martynas7928@gmail.com>
2019-09-13 11:04:08 +0300

    fix(module): Fix interfaces not being exported.

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,3 @@
 export * from './src/ngx-date-range-picker/module/ngx-date-range-picker.module';
 export * from './src/ngx-date-range-picker/component/ngx-date-range-picker.component';
+export * from './src/ngx-date-range-picker/models';
